6b1b6ed4ae84206fdfac7a5627f152bba45b0430
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-09-18 12:28:19 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/Workflow.ts
+++ kindeSrc/environment/workflows/Workflow.ts
@@ -40,8 +40,9 @@ export default async function NonPersistentSessionWorkflow(
     console.log("Non-persistent connection IDs:", nonPersistentConnectionIDs);
     console.log("Current login connectionId:", connectionId);
     console.log("kinde.ssoSession object:", kinde.ssoSession);
-
-    if (nonPersistentConnectionIDs.includes(connectionId)) {
+//kinde.ssoSession.setPolicy("non_persistent");
+    
+    if (nonPersistentConnectionIDs.includes(event.context.auth.connectionId)) {
       try {
         console.log("Matched connection, setting SSO session policy to non_persistent");
         kinde.ssoSession.setPolicy("non_persistent");
