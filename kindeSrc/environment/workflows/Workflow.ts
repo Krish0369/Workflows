7b0ed25720e432de22671f2a1f2aabb873246b0a
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-11-10 13:44:16 +1100

    Update Workflow.ts

--- kindeSrc/environment/workflows/Workflow.ts
+++ kindeSrc/environment/workflows/Workflow.ts
@@ -1,25 +1,29 @@
-import {
-	invalidateFormField
-} from "@kinde/infrastructure";
+import { invalidateFormField } from "@kinde/infrastructure";
 
 export const workflowSettings = {
-	id: "onUsernameProvided",
-	name: 'Validate username',
-	trigger: 'user:new_username_provided',
-	failurePolicy: {
-		action: "stop",
-	},
-	bindings: {
-		"kinde.widget": {}, // Required for accessing the UI
-	},
+  id: "onUsernameProvided",
+  name: "Validate username",
+  trigger: "user:new_username_provided",
+  failurePolicy: {
+    action: "stop",
+  },
+  bindings: {
+    "kinde.widget": {}, // Required for accessing the UI
+  },
 };
 
-//export default async function Workflow(event: any) {
-	//invalidateFormField('p_username', 'username format sucks, do better!')
-//}
 export default async function Workflow(event: any) {
+  console.log("Workflow triggered: onUsernameProvided");
+  console.log("Event received:", JSON.stringify(event, null, 2));
+
   const banned = ["admin", "root", "test"];
+  console.log("Banned usernames:", banned);
+  console.log("Username supplied by user:", event.suppliedUsername);
+
   if (banned.includes(event.suppliedUsername)) {
+    console.log("Username is banned. Invalidating form field.");
     invalidateFormField("p_username", "This username is not allowed.");
+  } else {
+    console.log("Username is allowed.");
   }
 }
