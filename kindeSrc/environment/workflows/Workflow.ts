4337c17f3db7858612b4a1683f3442bf2682e60a
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-11-10 13:32:22 +1100

    Update Workflow.ts

--- kindeSrc/environment/workflows/Workflow.ts
+++ kindeSrc/environment/workflows/Workflow.ts
@@ -1,22 +1,19 @@
 import {
-  WorkflowSettings,
-  WorkflowTrigger,
-  invalidateFormField
+	invalidateFormField
 } from "@kinde/infrastructure";
 
-
 export const workflowSettings = {
-  id: "validateUsername",
-  name: "Username validation",
-  trigger: "user:new_username_provided",
-  failurePolicy: { action: "stop" },
-  bindings: { "kinde.widget": {} },
+	id: "onUsernameProvided",
+	name: 'Validate username',
+	trigger: 'user:new_username_provided',
+	failurePolicy: {
+		action: "stop",
+	},
+	bindings: {
+		"kinde.widget": {}, // Required for accessing the UI
+	},
 };
 
-export default async function Workflow(event) {
-  const banned = ["admin", "root", "test"];
-  if (banned.includes(event.suppliedUsername)) {
-    invalidateFormField("p_username", "This username is not allowed.");
-  }
+export default async function Workflow(event: any) {
+	invalidateFormField('p_username', 'username format sucks, do better!')
 }
-
